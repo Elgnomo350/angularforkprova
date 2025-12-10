7735afe235f0eed3b5ff43bc5c4125f4440b4084
yeadr <yussef.elalami@institutvidreres.cat>
2025-12-10 09:20:56 +0100

    Ex 11

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,14 @@ import { Ex3 } from './ex3/ex3';
 import { Ex4 } from './ex4/ex4';
 import { Ex5 } from './ex5/ex5';
 import { Ex6 } from './ex6/ex6';
+import { Ex7 } from './ex7/ex7';
+import { Ex102 } from './ex102/ex102';
+import { Ex101 } from './ex101/ex101';
+import { Ex11 } from './ex11/ex11';
 
 @Component({
   selector: 'app-root',
-  imports: [Ex1, Ex2, Ex3, Ex4, Ex5, Ex6],
+  imports: [Ex1, Ex2, Ex3, Ex4, Ex5, Ex6, Ex7, Ex101, Ex102, Ex11],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
